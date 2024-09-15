cc6ad3c957d2e1a28a4b7d2add1aa6faad1ef605
Eric Koghene <131318577+Duroy51@users.noreply.github.com>
2024-09-15 18:21:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import LoginPage from "./components/Authentication/Login/LoginPage";
 import {AppRoute} from "./router/appRouter";
 import {BrowserRouter} from "react-router-dom";
 
